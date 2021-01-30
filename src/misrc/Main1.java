ae7e48beb5aa149d6f91cfa4261a3fcdecaf0cca
Nexu <alexandrunicolae.neacsu@campusfp.es>
2021-01-30 12:33:43 +0100

    1Snapshot main1.java

---
+++ src/misrc/Main1.java
@@ -0,0 +1,19 @@
+package misrc;
+
+
+public class Main1 {
+
+    
+    public static void main(String[] args) {
+        String[] nombre = {"Luis", "Miguel", "Carlos"};
+        for(int i=0; i<nombre.length; i++){
+            System.out.println(nombre[i]);
+        }
+        //Declaración array tipo double
+        double[] estatura = {1.72, 1.75, 1.65};
+        for(int i=0; i<estatura.length; i++){
+            System.out.println(estatura[i]);
+        }
+    }
+    
+}
